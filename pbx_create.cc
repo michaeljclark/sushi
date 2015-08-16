c86748bc618791a4585d071f96659b337769704a
Michael Clark <michael@metaparadigm.com>
2015-08-16 21:25:00 +1200

    Add sdk argument to createEmptyProject

--- pbx_create.cc
+++ pbx_create.cc
@@ -25,7 +25,7 @@
 
 int main(int argc, char **argv) {
 	XcodeprojPtr xcodeproj = std::make_shared<Xcodeproj>();
-	xcodeproj->createEmptyProject("test");
+	xcodeproj->createEmptyProject("test", "macosx");
 	PBXWriter::write(xcodeproj, std::cout, 0);
 	std::cout << std::endl;
 }
