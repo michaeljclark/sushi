00a204823896979042640108ffea62439b172e81
Michael Clark <michael@metaparadigm.com>
2015-08-30 13:29:36 +1200

    whitespace changes

--- sushi/project_xcode.cc
+++ sushi/project_xcode.cc
@@ -42,13 +42,15 @@ lib_output_data project_xcode::lib_output(project_root_ptr root, project_lib_ptr
 		lib_type = lib->lib_type;
 	}
 	if (lib_type == "static") {
-		return lib_output_data{ PBXFileReference::type_library_archive,
-								PBXNativeTarget::type_library_static,
-								"lib" + lib->lib_name + ".a"};
+		return lib_output_data {
+			PBXFileReference::type_library_archive,
+			PBXNativeTarget::type_library_static,
+			"lib" + lib->lib_name + ".a"};
 	} else {
-		return lib_output_data{ PBXFileReference::type_library_dylib,
-								PBXNativeTarget::type_library_dynamic,
-								lib->lib_name + ".dylib"};
+		return lib_output_data {
+			PBXFileReference::type_library_dylib,
+			PBXNativeTarget::type_library_dynamic,
+			lib->lib_name + ".dylib"};
 	}
 }
 
@@ -57,7 +59,7 @@ std::vector<std::string> project_xcode::lib_deps(project_root_ptr root, std::vec
 	std::vector<std::string> lib_deps;
 	for (auto lib_name : libs) {
 		auto li = std::find_if(root->lib_list.begin(), root->lib_list.end(),
-							   [&lib_name](project_lib_ptr const& lib) { return lib->lib_name == lib_name; });
+			[&lib_name](project_lib_ptr const& lib) { return lib->lib_name == lib_name; });
 		if (li != root->lib_list.end()) {
 			lib_deps.push_back(lib_output(root, *li).output_file);
 		} else {
@@ -78,10 +80,13 @@ XcodeprojPtr project_xcode::create_project(project_root_ptr root)
 	for (auto lib : root->lib_list) {
 		if (lib->lib_name == "*") continue;
 		auto lib_data = lib_output(root, lib);
-		xcodeproj->createNativeTarget(lib->lib_name, lib_data.output_file,
-									lib_data.file_type,
-									lib_data.target_type,
-									{ }, lib->source);
+		xcodeproj->createNativeTarget(
+			lib->lib_name,
+			lib_data.output_file,
+			lib_data.file_type,
+			lib_data.target_type,
+			{ },
+			lib->source);
 	}
 
 	// create tools
@@ -96,10 +101,13 @@ XcodeprojPtr project_xcode::create_project(project_root_ptr root)
 		if (tool->tool_name == "*") continue;
 		std::vector<std::string> tool_libs = lib_deps(root, tool->libs);
 		tool_libs.insert(tool_libs.end(), base_libs.begin(), base_libs.end());
-		xcodeproj->createNativeTarget(tool->tool_name, tool->tool_name,
-									PBXFileReference::type_executable,
-									PBXNativeTarget::type_tool,
-									tool_libs, tool->source);
+		xcodeproj->createNativeTarget(
+			tool->tool_name,
+			tool->tool_name,
+			PBXFileReference::type_executable,
+			PBXNativeTarget::type_tool,
+			tool_libs,
+			tool->source);
 	}
 
 	return xcodeproj;
